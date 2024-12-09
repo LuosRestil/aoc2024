d879fb9cb3dc854aff2118e0def8b1a4a2052bc0
LuosRestil <briansmithwebdeveloper@gmail.com>
2024-12-08 22:44:03 -0500

    optimize day06.js

--- day06.js
+++ day06.js
@@ -15,62 +15,73 @@ const turns = {
   ">": "v",
 };
 
-const filename = "inputs/day06.txt";
+const filepath = "inputs/day06.txt";
 
-const p1Grid = runPt1();
-console.log(`Part 1: ${countTrodden(p1Grid)}`);
+const path = runPt1();
+console.log(`Part 1: ${Object.keys(path).length}`);
 
-let trodden = [];
-for (let i = 0; i < p1Grid.length; i++) {
-  for (let j = 0; j < p1Grid[0].length; j++) {
-    if (turns[p1Grid[i][j]]) {
-      trodden.push([i, j]);
-    }
-  }
-}
-runPt2(trodden);
+runPt2(Object.keys(path));
 
 console.timeEnd("time");
 
 function runPt1() {
-  const grid = fs
-    .readFileSync("inputs/day06.txt", { encoding: "utf-8" })
-    .split("\n")
-    .map((line) => line.split(""));
-  solve(grid);
-  return grid;
+  const grid = getGrid();
+  const path = solve(grid);
+  return path;
 }
 
 function runPt2(trodden) {
-  const grid = fs
-    .readFileSync("inputs/day06.txt", { encoding: "utf-8" })
-    .split("\n")
-    .map((line) => line.split(""));
-  const guardPos = getGuardPos(grid);
+  const grid = getGrid();
+  const guard = getGuard(grid);
   let count = 0;
-  for (let pos of trodden) {
-    if (pos[0] === guardPos[0] && pos[1] === guardPos[1]) continue;
-    const copy = JSON.parse(JSON.stringify(grid));
-    copy[pos[0]][pos[1]] = "#";
-    const completed = solve(copy);
+  for (let key of trodden) {
+    const pos = key.split(':').map(num => Number(num));
+    if (pos[0] === guard.pos[0] && pos[1] === guard.pos[1])   
+      continue;
+    grid[pos[0]][pos[1]] = "#";
+    const completed = solve(grid);
     if (!completed) count++;
+    grid[pos[0]][pos[1]] = ".";
   }
   console.log(`Part 2: ${count}`);
 }
 
 function solve(grid) {
-  const guardPos = getGuardPos(grid);
-  const path = {};
-  while (isInGrid(grid, guardPos)) {
-    if (!tick(grid, guardPos, path)) return false;
+  const guard = getGuard(grid);
+  const path = {
+    [`${guard.pos[0]}:${guard.pos[1]}`]: ["^"],
+  };
+  while (isInGrid(grid, guard.pos)) {
+    if (!tick(grid, guard, path)) return null;
+  }
+  return path;
+}
+
+function tick(grid, guard, path) {
+  move(grid, guard);
+  const pathKey = `${guard.pos[0]}:${guard.pos[1]}`;
+  if (path[pathKey] && path[pathKey].includes(guard.dir)) return false;
+  if (isInGrid(grid, guard.pos)) {
+    path[pathKey] = path[pathKey] ? path[pathKey] : [];
+    path[pathKey].push(guard.dir);
   }
   return true;
 }
 
-function getGuardPos(grid) {
+function move(grid, guard) {
+  const nextPosMod = nextPosMods[guard.dir];
+  const nextPos = [guard.pos[0] + nextPosMod[0], guard.pos[1] + nextPosMod[1]];
+  if (isInGrid(grid, nextPos) && grid[nextPos[0]][nextPos[1]] == "#") {
+    guard.dir = turns[guard.dir];
+  } else {
+    guard.pos = nextPos;
+  }
+}
+
+function getGuard(grid) {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "^") return [i, j];
+      if (grid[i][j] === "^") return { pos: [i, j], dir: "^" };
     }
   }
 }
@@ -84,43 +95,9 @@ function isInGrid(grid, pos) {
   );
 }
 
-function tick(grid, pos, path) {
-  const dir = grid[pos[0]][pos[1]];
-  move(grid, pos, dir);
-  const pathKey = `${pos[0]}:${pos[1]}`;
-  if (path[pathKey] && path[pathKey].includes(grid[pos[0]][pos[1]]))
-    return false;
-  try {
-    path[pathKey] = path[pathKey] ? path[pathKey] : [];
-    path[pathKey].push(grid[pos[0]][pos[1]]);
-  } catch (err) {}
-  return true;
-}
-
-function move(grid, pos, dir) {
-  const nextPosMod = nextPosMods[dir];
-  const nextPos = [pos[0] + nextPosMod[0], pos[1] + nextPosMod[1]];
-  let hasTurned = false;
-  if (isInGrid(grid, nextPos)) {
-    if (grid[nextPos[0]][nextPos[1]] === "#") {
-      grid[pos[0]][pos[1]] = turns[dir];
-      hasTurned = true;
-    } else {
-      grid[nextPos[0]][nextPos[1]] = dir;
-    }
-  }
-  if (!hasTurned) {
-    pos[0] = nextPos[0];
-    pos[1] = nextPos[1];
-  }
-}
-
-function countTrodden(grid) {
-  let count = 0;
-  for (let row of grid) {
-    for (let col of row) {
-      if (Object.keys(turns).includes(col)) count++;
-    }
-  }
-  return count;
+function getGrid() {
+  return fs
+    .readFileSync(filepath, { encoding: "utf-8" })
+    .split("\n")
+    .map((line) => line.split(""));
 }
