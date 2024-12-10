533f2d7072b007a0ced775e45d5a701b7572fad9
LuosRestil <briansmithwebdeveloper@gmail.com>
2024-12-10 13:34:12 -0500

    day10 refactor

--- day10.js
+++ day10.js
@@ -7,35 +7,28 @@ const map = fs
   .split("\n")
   .map((row) => row.split("").map((col) => Number(col)));
 
-let total = 0;
+let p1Total = 0;
+let p2Total = 0;
 for (let row = 0; row < map.length; row++) {
   for (let col = 0; col < map[0].length; col++) {
     if (map[row][col] === 0) {
-      total += ninesReachable([row, col]);
+      const nines = getNines([row, col]);
+      p1Total += (new Set(nines)).size;
+      p2Total += nines.length;
     }
   }
 }
-console.log(`Part 1: ${total}`);
+console.log(`Part 1: ${p1Total}`);
+console.log(`Part 2: ${p2Total}`);
 
-total = 0;
-for (let row = 0; row < map.length; row++) {
-  for (let col = 0; col < map[0].length; col++) {
-    if (map[row][col] === 0) {
-      const memo = {};
-      total += uniqueTrails(row, col, memo);
-    }
-  }
-}
-console.log(`Part 2: ${total}`);
-
-function ninesReachable(start) {
-  const discovered = new Set();
+function getNines(start) {
+  const discovered = [];
   const queue = [start];
   while (queue.length > 0) {
     const pos = queue.pop();
     const [row, col] = pos;
     const val = map[row][col];
-    if (val === 9) discovered.add(`${row}:${col}`);
+    if (val === 9) discovered.push(`${row}:${col}`);
     // up
     if (isOnMap(row - 1, col) && map[row - 1][col] === val + 1)
       queue.push([row - 1, col]);
@@ -49,41 +42,11 @@ function ninesReachable(start) {
     if (isOnMap(row, col + 1) && map[row][col + 1] === val + 1)
       queue.push([row, col + 1]);
   }
-  return discovered.size;
-}
-
-function uniqueTrails(row, col, memo) {
-  const val = map[row][col];
-  if (val === 9) return 1;
-
-  const memoKey = getMemoKey(row, col);
-  const memoVal = memo[memoKey];
-  if (memoVal) return memoVal;
-
-  let total = 0;
-  // up
-  if (isOnMap(row - 1, col) && map[row - 1][col] === val + 1)
-    total += uniqueTrails(row - 1, col, memo);
-  // down
-  if (isOnMap(row + 1, col) && map[row + 1][col] === val + 1)
-    total += uniqueTrails(row + 1, col, memo);
-  // left
-  if (isOnMap(row, col - 1) && map[row][col - 1] === val + 1)
-    total += uniqueTrails(row, col - 1, memo);
-  // right
-  if (isOnMap(row, col + 1) && map[row][col + 1] === val + 1)
-    total += uniqueTrails(row, col + 1, memo);
-
-  memo[memoKey] = total;
-  return total;
+  return discovered;
 }
 
 function isOnMap(row, col) {
   return row >= 0 && row < map.length && col >= 0 && col < map[0].length;
-}
-
-function getMemoKey(row, col) {
-  return `${row}:${col}`;
 }
 
 console.timeEnd("time");
